3f8efa9b6a9a8647a7968bd0d8fea3535f26a9bf
EP <masogin@gmail.comD>
2017-10-14 12:51:25 +0300

    half an adaptive

--- js/parallax.js
+++ js/parallax.js
@@ -5,27 +5,24 @@
 //3 - 2400 - 2828
 (function(){
     var firstImage = document.querySelector('.parallax-one__img');
-        var paraBlockImage = document.querySelector('.parallax-one');
+    var paraBlockImage = document.querySelector('.parallax-one');
     var scrollPosition;
     var position=0;
     var projectsBlock = document.querySelector(".projects");
     var rectObject;
-    var height = 300;
+    var logoSlider = document.querySelector('.logo-slider');
+    var customers = document.querySelector('.customers');
+    var blog = document.querySelector('.blog');
+    var buyFromUs = document.querySelector('.buy-from-us');
+    var footer = document.getElementsByTagName('footer')[0];
+  
     window.addEventListener('scroll', function (){
-	scrollPosition = window.pageYOffset;
-	console.log(scrollPosition);
-	rectObject = firstImage.getBoundingClientRect().top ;
-	console.log(rectObject+" rect ");
+//	scrollPosition = window.pageYOffset;
+//	console.log(scrollPosition);
+//	rectObject = firstImage.getBoundingClientRect().top ;
+//	console.log(rectObject+" rect ");
 	
-	if(rectObject <= 300 ) {
-	    
-	    position = (rectObject)/10 ;
-	    console.log("position " + position);
-	    firstImage.style.backgroundPositionY = position+"px" ;
-	}
-	else if (scrollPosition > 1200) {
-	     firstImage.style.backgroundPositionY = 0+"px" ;
-	}
+	
     });
     
 })();
